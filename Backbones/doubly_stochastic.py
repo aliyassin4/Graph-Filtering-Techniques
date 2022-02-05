13962fca50c3a47991da91b93fab89d673179952
Ali Yassin <aliyassin4@hotmail.com>
2022-02-05 19:22:59 +0100

    polya filter added

--- Backbones/doubly_stochastic.py
+++ Backbones/doubly_stochastic.py
@@ -31,9 +31,9 @@ def read(filename, column_of_interest, triangular_input = False, consider_self_l
    """
    table = pd.read_csv(filename, sep = sep)
    table = table[["source", "target", column_of_interest]]
-   table.rename(columns = {column_of_interest: "nij"}, inplace = True)
+   table.rename(columns = {column_of_interest: "weight"}, inplace = True)
    if drop_zeroes:
-      table = table[table["nij"] > 0]
+      table = table[table["weight"] > 0]
    if not consider_self_loops:
       table = table[table["source"] != table["target"]]
    if triangular_input:
@@ -57,7 +57,7 @@ def doubly_stochastic(table, undirected = False, return_self_loops = False):
    table = table.copy()
    table2 = table.copy()
    original_nodes = len(set(table["source"]) | set(table["target"]))
-   table = pd.pivot_table(table, values = "nij", index = "source", columns = "target", aggfunc = "sum", fill_value = 0) + .001
+   table = pd.pivot_table(table, values = "weight", index = "source", columns = "target", aggfunc = "sum", fill_value = 0) + .0001
    row_sums = table.sum(axis = 1)
    attempts = 0
    while np.std(row_sums) > 1e-12:
@@ -71,31 +71,64 @@ def doubly_stochastic(table, undirected = False, return_self_loops = False):
          return pd.DataFrame()
    table = pd.melt(table.reset_index(), id_vars = "source")
    table = table[table["source"] < table["target"]]
-   table = table[table["value"] >= 0].sort_values(by = "value", ascending = False)
+   table = table[table["value"] > 0].sort_values(by = "value", ascending = False)
+   table = table.merge(table2[["source", "target", "weight"]], on = ["source", "target"])
    i = 0
    if undirected:
       G = nx.Graph()
-      while nx.number_connected_components(G) != 1 or nx.number_of_nodes(G) < original_nodes:
+      while nx.number_connected_components(G) != 1 or nx.number_of_nodes(G) < original_nodes or nx.is_connected(G) == False:
          edge = table.iloc[i]
          G.add_edge(edge["source"], edge["target"], weight = edge["value"])
-         table.loc[table.loc[(table['source'] == edge["source"]) & (table['target'] == edge["target"])].index[0],'ds_backbone'] = True
+         table.loc[table.loc[(table['source'] == edge["source"]) & (table['target'] == edge["target"])].index[0],'ds_backbone'] = True 
          i += 1
    else:
       G = nx.DiGraph()
-      while nx.number_weakly_connected_components(G) != 1 or nx.number_of_nodes(G) < original_nodes:
+      while nx.number_weakly_connected_components(G) != 1 or nx.number_of_nodes(G) < original_nodes or nx.is_connected(G) == False:
          edge = table.iloc[i]
          G.add_edge(edge["source"], edge["target"], weight = edge["value"])
-         table.loc[table.loc[(table['source'] == edge["source"]) & (table['target'] == edge["target"])].index[0],'ds_backbone'] = True
-
+         table.loc[table.loc[(table['source'] == edge["source"]) & (table['target'] == edge["target"])].index[0],'ds_backbone'] = True 
          i += 1
-#    table1 = pd.melt(nx.to_pandas_adjacency(G).reset_index(), id_vars = "index")
+   #table = pd.melt(nx.to_pandas_adjacency(G).reset_index(), id_vars = "index")
    table = table[table["value"] >= 0]
-   table.rename(columns = {"index": "source", "variable": "target", "value": "cij"}, inplace = True)
-   table["score"] = table["cij"]
-   table = table.merge(table2[["source", "target", "nij"]], on = ["source", "target"])
+   table.rename(columns = {"index": "source", "variable": "target", "value": "score"}, inplace = True)
+   table = table.fillna(False) 
    if not return_self_loops:
       table = table[table["source"] != table["target"]]
    if undirected:
       table = table[table["source"] <= table["target"]]
-   table = table.fillna(False)
-   return table[["source", "target", "nij", "score", "ds_backbone"]]
+   #nx.draw(G)     
+   return table[["source", "target", "weight", "score", "ds_backbone"]]
+
+
+#another method
+#from Backbones.doubly_stochastic import read#, doubly_stochastic as ds
+
+# original_table, nnodes, nnedges = read("../Datasets/lesmis.csv", "weight", sep=',', consider_self_loops=False, triangular_input = True, undirected=True) 
+
+# original_nodes = len(set(original_table["source"]) | set(original_table["target"]))
+# table = pd.pivot_table(original_table, values = "weight", index = "source", columns = "target", aggfunc = "sum", fill_value = 0) + 0.0001
+
+# from sinkhorn_knopp import sinkhorn_knopp as skp
+# sk = skp.SinkhornKnopp()
+# table_ds = sk.fit(table)
+# table = pd.DataFrame(data=table_ds, index=table.index, columns=table.columns)
+# table = table.stack().reset_index()
+# table.rename(columns = {'source':'source','target':'target',0:'score'}, inplace=True)
+# table = table[table['source'] != table['target']]
+# table = table[table['source'] < table['target']]
+
+# table = table.merge(original_table, on = ["source", "target"])
+
+
+# table = table.sort_values(by = 'score', ascending = False)
+
+# i=0
+# G = nx.Graph()
+# while True:
+#         edge = table.iloc[i]
+#         G.add_edge(edge["source"], edge["target"], weight = edge["score"])
+#         #table.loc[table.loc[(table['source'] == edge["source"]) & (table['target'] == edge["target"])].index[0],'ds_backbone'] = True
+#         i = i+1
+#         if nx.is_connected(G) and nx.number_of_nodes(G) == original_nodes:
+#             break
+# nx.to_pandas_edgelist(G)
